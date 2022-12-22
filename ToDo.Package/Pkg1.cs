205299e336304842d57a73bdcc6dae4f6dd4e856
Nathan Praja Kusuma <92840347+xjagaimo@users.noreply.github.com>
2022-12-22 21:13:12 +0700

    Add project files.

---
+++ ToDo.Package/Pkg1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo.Package
+{
+    public class Pkg1
+    {
+        public void updateApp(int mili)
+        {
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(timer_tick);
+            //5 seconds
+            timer.Interval = mili;
+            timer.Start();
+        }
+
+        public void timer_tick(object sender, EventArgs ev)
+        {
+            
+        }
+
+        public bool testTime(DateTime date)
+        {
+            if ((date - DateTime.Now).TotalSeconds <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public DateTime dateConverter(string date)
+        {
+            return DateTime.ParseExact(date, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+        }
+    }
+
+    
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToDo.Package;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Pkg1 pkg = new Pkg1();
+            DateTime date1 = new DateTime(2010, 10, 22, 10, 0, 0);
+            //bool worked = false;
+            Assert.IsTrue(pkg.testTime(date1));
+            //Assert.AreEqual(worked, pkg.testTime(date1));
+            
+        }
+    }
+}
